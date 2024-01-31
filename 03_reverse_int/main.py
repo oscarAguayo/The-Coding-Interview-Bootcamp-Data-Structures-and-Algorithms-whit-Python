8e79ad59f5dcae24aa0d24d84b31f13d40741db9
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-01-31 11:31:35 -0600

    Merge: 03_reverse_int/

---
+++ 03_reverse_int/main.py
@@ -0,0 +1,14 @@
+def reverse_int(n: int) -> int:
+# --- Directions
+# Given an integer, return an integer that is the reverse
+# ordering of numbers.
+# --- Examples
+#   reverse_int(15) == 51
+#   reverse_int(981) == 189
+#   reverse_int(500) == 5
+#   reverse_int(-15) == -51
+#   reverse_int(-90) == -9
+    pass
+
+if __name__ == "__main__":
+    print(f"Reverse is: {reverse_int(int(input('Given a integer: ')))}")
