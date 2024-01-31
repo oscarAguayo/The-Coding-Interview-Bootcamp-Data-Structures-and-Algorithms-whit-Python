a2514a1177b05367673a0aeb939b690b7598c1b2
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-01-31 10:04:50 -0600

    Add exercise 02_palindrome

---
+++ 02_palindrome/main.py
@@ -0,0 +1,13 @@
+def palindrome(text: str):
+# --- Directions
+# Given a string, return true if the string is a palindrome
+# or false if it is not.  Palindromes are strings that
+# form the same word if it is reversed. *Do* include spaces
+# and punctuation in determining if the string is a palindrome.
+# --- Examples:
+#   palindrome("abba") === true
+#   palindrome("abcdefg") === false
+    pass
+
+if __name__ == "__main__":
+    print(f"Is palindrome?: {palindrome(input('Given a palindrome: '))}")
