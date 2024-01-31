f435f4e4e44109df857d15b6349debb965d9b14c
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-01-31 10:07:01 -0600

    Add return object type in functions

--- 02_palindrome/main.py
+++ 02_palindrome/main.py
@@ -1,12 +1,13 @@
-def palindrome(text: str):
+def palindrome(text: str) -> bool:
 # --- Directions
 # Given a string, return true if the string is a palindrome
 # or false if it is not.  Palindromes are strings that
 # form the same word if it is reversed. *Do* include spaces
 # and punctuation in determining if the string is a palindrome.
 # --- Examples:
-#   palindrome("abba") === true
-#   palindrome("abcdefg") === false
+#   palindrome("abba") == True
+#   palindrome("abcdefg") == False
+#   palindrome("Anita lava la tina") == True
     pass
 
 if __name__ == "__main__":
