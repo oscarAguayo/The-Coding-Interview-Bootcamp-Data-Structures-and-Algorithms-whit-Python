59b1c1710e5ba90764b2ca422250a3b6fcbeb506
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-01-31 11:12:17 -0600

    Improve the solution 02_palindrome and add another test cases

--- 02_palindrome/main.py
+++ 02_palindrome/main.py
@@ -1,3 +1,5 @@
+import re
+
 def palindrome(text: str) -> bool:
 # --- Directions
 # Given a string, return true if the string is a palindrome
@@ -5,10 +7,13 @@ def palindrome(text: str) -> bool:
 # form the same word if it is reversed. *Do* include spaces
 # and punctuation in determining if the string is a palindrome.
 # --- Examples:
-#   palindrome("abba") == True
-#   palindrome("abcdefg") == False
 #   palindrome("Anita lava la tina") == True
-    normalize_text = text.replace(' ', '').lower()
+#   palindrome("Amó la paloma") == True
+#   palindrome("¡Ojo! corre poco perro cojo") == True
+#   palindrome("Allí ni cocina, ni cocinilla") == True
+    text = text.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
+    pattern = '[^a-zA-Z]'
+    normalize_text = re.sub(pattern, '', text).lower()
     return normalize_text == ''.join(list(normalize_text)[::-1])
 
 if __name__ == "__main__":
--- 02_palindrome/test_main.py
+++ 02_palindrome/test_main.py
@@ -1,6 +1,7 @@
 import main
 
 def test_palindrome():
-    assert main.palindrome("abba") == True
-    assert main.palindrome("abcdefg") == False
     assert main.palindrome("Anita lava la tina") == True
+    assert main.palindrome("Amó la paloma") == True
+    assert main.palindrome("¡Ojo! corre poco perro cojo") == True
+    assert main.palindrome("Allí ni cocina, ni cocinilla") == True
