6fbc5734da054b1b5ed5f815cc3d2ec4f477da19
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-02-01 03:29:35 -0600

    Edit the solution of 02_palindrome

--- 02_palindrome/main.py
+++ 02_palindrome/main.py
@@ -1,5 +1,3 @@
-import re
-
 def palindrome(text: str) -> bool:
 # --- Directions
 # Given a string, return true if the string is a palindrome
@@ -7,14 +5,10 @@ def palindrome(text: str) -> bool:
 # form the same word if it is reversed. *Do* include spaces
 # and punctuation in determining if the string is a palindrome.
 # --- Examples:
-#   palindrome("Anita lava la tina") == True
-#   palindrome("Amó la paloma") == True
-#   palindrome("¡Ojo! corre poco perro cojo") == True
-#   palindrome("Allí ni cocina, ni cocinilla") == True
-    text = text.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
-    pattern = '[^a-zA-Z]'
-    normalize_text = re.sub(pattern, '', text).lower()
-    return normalize_text == ''.join(list(normalize_text)[::-1])
+#   palindrome("abba") == True
+#   palindrome("abcdefg") == False
+#   palindrome("Fish hsif") == False
+    return text == ''.join(list(text)[::-1])
 
 if __name__ == "__main__":
     print(f"Is palindrome?: {palindrome(input('Given a palindrome: '))}")
