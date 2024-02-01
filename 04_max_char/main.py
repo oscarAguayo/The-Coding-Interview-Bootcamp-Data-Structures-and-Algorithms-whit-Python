5c5d16afe4d1a6d8d742dc3a9b35cb562aa410d6
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-02-01 06:05:42 -0600

    Add solution to 04_max_char exercise

--- 04_max_char/main.py
+++ 04_max_char/main.py
@@ -5,8 +5,9 @@
 ## max_char("abcccccccd") == "c"
 ## max_char("apple 1231111") == "1"
 
-def max_char(text: str) -> str:
-    pass
+def max_char(text: str) -> dict:
+    data = {text.count(char): char for char in set(text)}
+    return data.get(max(data))
 
 if __name__ == "__main__":
     print(f"The most character commonly used in string is: {max_char(input('Give a string: '))}")
