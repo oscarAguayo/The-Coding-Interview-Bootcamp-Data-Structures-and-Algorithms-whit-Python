0a2891f3b9431caeeffc49c6a188ba43bc34caed
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-02-01 07:12:50 -0600

    Add exercise 05_fizz_buzz

---
+++ 05_fizz_buzz/main.py
@@ -0,0 +1,20 @@
+## --- Directions
+## Write a program that ~~console logs~~ put in list the numbers
+## from 1 to n. But for multiples of three print
+## “fizz” instead of the number and for the multiples
+## of five print “buzz”. For numbers which are multiples
+## of both three and five print “fizzbuzz”.
+## --- Example
+##   fizzBuzz(5);
+##   1
+##   2
+##   fizz
+##   4
+##   buzz
+
+def fizz_buzz(number: int) -> list:
+    pass
+
+if __name__ == "__main__":
+    for number in fizz_buzz(int(input("Give a number: "))):
+        print(number)
---
+++ 05_fizz_buzz/test_main.py
@@ -0,0 +1,25 @@
+import main
+
+def test_length_of_5_elements():
+    data = main.fizz_buzz(5)
+    assert len(data) == 5
+
+def test_fizz_buzz_values_of_15():
+    data = main.fizz_buzz(15)
+    assert data[0] == 1
+    assert data[1] == 2
+    assert data[2] == "fizz"
+    assert data[3] == 4
+    assert data[4] == "buzz"
+    assert data[5] == "fizz"
+    assert data[6] == 7
+    assert data[7] == 8
+    assert data[8] == "fizz"
+    assert data[9] == "buzz"
+    assert data[10] == 11
+    assert data[11] == "fizz"
+    assert data[12] == 13
+    assert data[13] == 14
+    assert data[14] == "fizzbuzz"
+    for i in data:
+        print(i)
