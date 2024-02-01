2690c47e48760489cf06ed9356b969d3cd24b9d4
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-02-01 03:08:39 -0600

    Merge: test files updated

--- 02_palindrome/test_main.py
+++ 02_palindrome/test_main.py
@@ -1,7 +1,12 @@
 import main
 
 def test_palindrome():
-    assert main.palindrome("Anita lava la tina") == True
-    assert main.palindrome("Amó la paloma") == True
-    assert main.palindrome("¡Ojo! corre poco perro cojo") == True
-    assert main.palindrome("Allí ni cocina, ni cocinilla") == True
+    assert main.palindrome("abba") == True
+    assert main.palindrome("abcdefg") == False
+    assert main.palindrome("aba") == True
+    assert main.palindrome(" aba") == False
+    assert main.palindrome("aba ") == False
+    assert main.palindrome("greetings") == False
+    assert main.palindrome("1000000001") == True
+    assert main.palindrome("Fish hsif") == False
+    assert main.palindrome("pennep") == True
--- 03_reverse_int/test_main.py
+++ 03_reverse_int/test_main.py
@@ -1,8 +1,19 @@
 import main
 
-def test_reverse_int():
+def test_zero():
+    assert main.reverse_int(0) == 0
+
+def test_reverse_positive_number():
     assert main.reverse_int(15) == 51
     assert main.reverse_int(981) == 189
     assert main.reverse_int(500) == 5
+    assert main.reverse_int(5) == 5
+    assert main.reverse_int(90) == 9
+    assert main.reverse_int(2359) == 9532
+
+
+def test_reverse_negative_number():
     assert main.reverse_int(-15) == -51
     assert main.reverse_int(-90) == -9
+    assert main.reverse_int(-5) == -5
+    assert main.reverse_int(-2359) == -9532
