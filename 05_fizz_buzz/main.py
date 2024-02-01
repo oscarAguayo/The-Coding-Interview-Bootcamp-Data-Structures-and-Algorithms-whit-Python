f01837280172021eb4ddf011f7a5406041fab643
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-02-01 08:21:38 -0600

    Add solution of 05_fizz_buzz exercise

--- 05_fizz_buzz/main.py
+++ 05_fizz_buzz/main.py
@@ -5,7 +5,7 @@
 ## of five print “buzz”. For numbers which are multiples
 ## of both three and five print “fizzbuzz”.
 ## --- Example
-##   fizzBuzz(5);
+##   fizz_buzz(5);
 ##   1
 ##   2
 ##   fizz
@@ -13,7 +13,17 @@
 ##   buzz
 
 def fizz_buzz(number: int) -> list:
-    pass
+    data = []
+    for i in range(1, number+1):
+        if i % 3 == 0 and not i % 5 == 0:
+            data.append('fizz')
+        elif i % 5 == 0 and not i % 3 == 0:
+            data.append('buzz')
+        elif i % 3 == 0 and i % 5 == 0:
+            data.append('fizzbuzz')
+        else:
+            data.append(i)
+    return data
 
 if __name__ == "__main__":
     for number in fizz_buzz(int(input("Give a number: "))):
