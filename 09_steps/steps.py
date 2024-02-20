c5970f095bf8dd67081303170f93e20cfa320e70
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-02-20 03:22:19 -0600

    Merge: 09_steps exercise

---
+++ 09_steps/steps.py
@@ -0,0 +1,24 @@
+# --- Directions
+# Write a function that accepts a positive number N.
+# The function should console log a step shape
+# with N levels using the # character.  Make sure the
+# step has spaces on the right hand side!
+# --- Examples
+#   steps(2)
+#       '# '
+#       '##'
+#   steps(3)
+#       '#  '
+#       '## '
+#       '###'
+#   steps(4)
+#       '#   '
+#       '##  '
+#       '### '
+#       '####'
+
+def steps(n: int) -> None:
+    pass
+
+if __name__ == "__main__":
+    steps(int(input("Give a positive number: ")))
